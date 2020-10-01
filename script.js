46045a25971204725d09fd7642a923ef27364b2a
FlavienBesseau <besseau.flavien@gmail.com>
2020-10-01 20:59:24 +0200

    Modif navbar

---
+++ script.js
@@ -0,0 +1,24 @@
+const checkboxElement = document.getElementById('menu-checkbox');
+const logoElement = document.getElementById('searchButton');
+const titleNavElement = document.getElementById('titleNav');    
+const navbarlistElement = document.getElementById('listDer');
+
+let burgerClose = true;
+
+checkboxElement.addEventListener('click',function(){
+    if (burgerClose){
+        logoElement.remove();
+        titleNavElement.remove();
+        const newliElement = document.createElement('li');
+        newliElement.innerHTML = "MyLovelyTrack";
+        const navbarlistElement = document.getElementById('listDer');
+        let nextSibling = document.getElementById("firstLi");
+        navbarlistElement.insertBefore(newliElement, nextSibling);
+        newliElement.style.fontSize = "2rem";
+        burgerClose = false;
+    }
+    else{
+        newliElement.remove();
+        burgerClose = true;
+    }     
+})
