83e237baa0b1693f22aac3d60ea750cd2cacb01d
FlavienBesseau <besseau.flavien@gmail.com>
2020-10-01 22:37:29 +0200

    navbar finie

--- script.js
+++ script.js
@@ -2,26 +2,27 @@ const checkboxElement = document.getElementById('menu-checkbox');
 const logoElement = document.getElementById('searchButton');
 const titleNavElement = document.getElementById('titleNav');    
 const navbarlistElement = document.getElementById('listDer');
+const navWrapperElement = document.getElementById('nav-wrapper');
+const newliElement = document.createElement('li');
+newliElement.innerHTML = "MyLovelyTrack";
 
 let burgerClose = true;
+
 
 checkboxElement.addEventListener('click',function(){
     if (burgerClose){
         logoElement.remove();
         titleNavElement.remove();
-        const newliElement = document.createElement('li');
-        newliElement.innerHTML = "MyLovelyTrack";
         const navbarlistElement = document.getElementById('listDer');
         let nextSibling = document.getElementById("firstLi");
         navbarlistElement.insertBefore(newliElement, nextSibling);
         newliElement.style.fontSize = "2rem";
-        burgerClose = false;
-        const newTitle = document.createElement('h2');
-        newliElement.innerHTML = "MyLovelyTrack";
+        burgerClose = false
     }
     else{
+        navWrapperElement.appendChild(titleNavElement);
+        navWrapperElement.appendChild(logoElement);
         newliElement.remove();
-        
         burgerClose = true;
     }     
 })
