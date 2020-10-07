6cf727e1a1a493136d7dd5e6392697a9e1b7338e
lilykr <lisa-lou.k@hotmail.fr>
2020-10-07 15:43:13 +0200

    fixed curly bracket JS

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+
 const buttonLDorElement = document.getElementById('buttonLivreDor');
 
 const inputLivreDor = document.getElementById('msgLiDor');
@@ -8,12 +14,13 @@ const nomLivreDor = document.getElementById('nom');
 const mainElement = document.querySelector('#livredor');
 
 
-// buttonLDorElement.addEventListener('click', function () {
-//     const newParagrapheElement = document.createElement('p');
-//     mainElement.appendChild(newParagrapheElement);
-//     newParagrapheElement.innerHTML = '- ' + nomLivreDor.value + ' a commenté: ' + "'" + inputLivreDor.value + "'";
-//     newParagrapheElement.style.padding = "12px";
-// })
+ buttonLDorElement.addEventListener('click', function () {
+     const newParagrapheElement = document.createElement('p');
+     mainElement.appendChild(newParagrapheElement);
+     newParagrapheElement.innerHTML = '- ' + nomLivreDor.value + ' a commenté: ' + "'" + inputLivreDor.value + "'";
+     newParagrapheElement.style.padding = "12px";
+})
+
 
 // START HAROLD - script for LOGIN BUTTON
 function openForm() {
