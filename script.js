a486b81e35ae11726c717ecb684d6f18d2dc4b22
FlavienBesseau <besseau.flavien@gmail.com>
2020-10-07 10:32:34 +0200

    Modifications de détails + respect normes html

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
 const buttonLDorElement = document.getElementById('buttonLivreDor');
 
@@ -13,18 +13,19 @@ const mainElement = document.querySelector('#livredor');
 
 
 buttonLDorElement.addEventListener('click', function () {
-    const newParagrapheElement = document.createElement('p');
-    mainElement.appendChild(newParagrapheElement);
-    newParagrapheElement.innerHTML = '- ' + nomLivreDor.value + ' a commenté: ' + "'" + inputLivreDor.value + "'";
-    newParagrapheElement.style.padding = "12px";
+  const newParagrapheElement = document.createElement('p');
+  mainElement.appendChild(newParagrapheElement);
+  newParagrapheElement.innerHTML = '- ' + nomLivreDor.value + ' a commenté: ' + "'" + inputLivreDor.value + "'";
+  newParagrapheElement.style.padding = "12px";
+  newParagrapheElement.style.textAlign = "Center";
 })
 
 // START HAROLD - script for LOGIN BUTTON
 function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 // END HAROLD - script for LOGIN BUTTON
