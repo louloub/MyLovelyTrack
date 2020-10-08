bbf4c49455d81b07d398776bd03ed7d2ef583af8
loulou <yourdj.france@gmail.com>
2020-10-08 15:43:06 +0200

    modification on script for button works

--- script.js
+++ script.js
@@ -17,13 +17,28 @@ function addComment() {
 
 
 // START HAROLD - script for LOGIN BUTTON
+
+let isPopupIsDisplayed = false; // variable for check if popup is display
+
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+    switch(isPopupIsDisplayed){
+        case true : // Popup is alwready display, i close it
+            document.getElementById("myForm").style.display = "none";
+            isPopupIsDisplayed = false;  
+            break;  
+        case false : // Popup is not displayed, i open it
+            document.getElementById("myForm").style.display = "block";
+            isPopupIsDisplayed = true;  
+            break;  
+        default : console.log("i'm in default switch onpenForm")         
+    }
 }
 
+// Button "fermer" into popup
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
 // END HAROLD - script for LOGIN BUTTON
 
 
